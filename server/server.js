b2d93d6dcea39e8d2e6fd38cd2975af153fd21e6
Brant McCarthy <brant.mccarthy@me.com>
2022-02-06 18:05:04 -0600

    updated PORT syntax for Heroku

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
